0c453b5e791417993a98a20b3e100ec1c329b7b0
PC-ROS <gronick.h@pm.me>
2023-10-01 20:50:26 -0500

    Añadiendo archivo README

---
+++ README.md
@@ -0,0 +1,38 @@
+"""
+# Función de Campana Gaussiana en Python
+
+Este es un simple programa en Python que calcula y grafica una función de campana gaussiana. 
+La campana gaussiana es una función matemática comúnmente utilizada en estadísticas y procesamiento de señales. 
+El programa permite a los usuarios personalizar la campana gaussiana ingresando la media, la desviación estándar y la amplitud.
+
+## Requisitos
+
+Asegúrate de tener instalado Python en tu sistema. También necesitarás las siguientes bibliotecas de Python, que puedes instalar utilizando `pip`:
+
+- NumPy: Para cálculos numéricos.
+- Matplotlib: Para crear gráficos.
+
+Puedes instalar estas bibliotecas ejecutando los siguientes comandos:
+
+```bash
+pip install numpy
+pip install matplotlib
+
+## Uso
+1. Clona o descarga este repositorio en tu máquina.
+2. Abre una terminal y navega al directorio donde se encuentra el archivo app.py.
+3. Ejecuta el programa utilizando Python: python app.py
+4. El programa te pedirá que ingreses la media (mu), la desviación estándar (sigma) y la amplitud de la campana gaussiana.
+5. Una vez ingresados los valores, el programa calculará la función de campana gaussiana y la mostrará en un gráfico.
+
+## Ejemplo
+Aquí tienes un ejemplo de cómo se ve el programa en acción:
+- Ingrese la media (mu): 0
+- Ingrese la desviación estándar (sigma): 1
+- Ingrese la amplitud: 1
+
+## Contribuciones
+Siéntete libre de contribuir a este proyecto abriendo problemas (issues) o enviando solicitudes de extracción (pull requests) para mejoras o correcciones.
+
+## Contribuciones
+Siéntete libre de contribuir a este proyecto abriendo problemas (issues) o enviando solicitudes de extracción (pull requests) para mejoras o correcciones.
--- app.py
+++ app.py
@@ -1,25 +1,37 @@
+# Importar la biblioteca NumPy para cálculos numéricos
 import numpy as np
 
+# Definir una función para la campana gaussiana con parámetros x, mu, sigma y amplitud
 def campana_gaussiana(x, mu, sigma, amplitud):
+    # Calcular el exponente de la campana
     exponente = -((x - mu) ** 2) / (2 * sigma ** 2)
+    
+    # Calcular el coeficiente de la campana gaussiana
     coeficiente = amplitud / (sigma * np.sqrt(2 * np.pi))
+    
+    # Calcular y devolver el valor de la campana gaussiana
     return coeficiente * np.exp(exponente)
 
-# Solicitar al usuario que ingrese la media, la desviación estándar y la amplitud
+# Solicitar al usuario que ingrese la media (mu), la desviación estándar (sigma) y la amplitud
 mu = float(input("Ingrese la media (mu): "))
 sigma = float(input("Ingrese la desviación estándar (sigma): "))
 amplitud = float(input("Ingrese la amplitud: "))
 
-# Crear un arreglo de valores x
+# Crear un arreglo de valores x que representan el dominio de la función
 x = np.linspace(-5, 5, 100)
 
 # Calcular la función de campana gaussiana con los valores ingresados por el usuario
 y = campana_gaussiana(x, mu, sigma, amplitud)
 
+# Importar la biblioteca Matplotlib para trazar gráficos
 import matplotlib.pyplot as plt
-plt.plot(x, y)
-plt.title('Función de Campana Gaussiana')
-plt.xlabel('x')
-plt.ylabel('f(x)')
-plt.grid(True)
+
+# Trazar la función de campana gaussiana
+plt.plot(x, y)  # Trazar los valores de x contra y
+plt.title('Función de Campana Gaussiana')  # Agregar un título al gráfico
+plt.xlabel('x')  # Etiqueta del eje x
+plt.ylabel('f(x)')  # Etiqueta del eje y
+plt.grid(True)  # Habilitar las líneas de la cuadrícula en el gráfico
+
+# Mostrar el gráfico resultante
 plt.show()
